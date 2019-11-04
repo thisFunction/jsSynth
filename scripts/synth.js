a4280fa61ae61988d5f73230996b7bdb44a0bf73
Adam Skoczylas <skocadam@gmail.com>
2019-11-03 20:42:29 -0500

    clean up code

--- scripts/synth.js
+++ scripts/synth.js
@@ -1,50 +1,95 @@
+
+
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+let context; 
+let oscillator; 
+let analyser;
+let gainNode;
+let octave = 261.63;
+
 window.onload = function() {
-
-    const AudioContext = window.AudioContext || window.webkitAudioContext;
-    const context = new AudioContext();
-
-    var oscillator = context.createOscillator();
-    oscillator.type = 'sine';
-    oscillator.frequency.value = 261.63;
-
-    const gainNode = context.createGain();
-
-    oscillator.connect(gainNode);
-
-    var analyser = context.createAnalyser();
-
-    gainNode.connect(analyser)
-
-    analyser.connect(context.destination);
-
+    setupCanvas();
     document.querySelector('.start').addEventListener('click', function() {
-        if(oscillator.context.state === 'suspended') {
+        if (!this.classList.contains('active')) {
+            createNoise();
             document.querySelector('.stop').classList.remove('active');
-            this.classList.add('active')
-            oscillator.start();
-        } else {
-            document.querySelector('.stop').classList.remove('active');
-            this.classList.add('active')
-            gainNode.gain.value = document.querySelector('.volume').value;
+            this.classList.add('active');
         }
-        
     });
 
     document.querySelector('.stop').addEventListener('click', function() {
         document.querySelector('.start').classList.remove('active');
         this.classList.add('active');
-        gainNode.gain.value = 0;
+        gainNode.gain.setValueAtTime(0, context.currentTime)
     });
+
+    function createNoise() {
+        context = new AudioContext();
+        oscillator = context.createOscillator();
+        oscillator.type = document.querySelector('.change-waveform').value;
+        oscillator.frequency.value = octave;
+
+        gainNode = context.createGain();
+        gainNode.gain.value = document.querySelector('.volume').value;
+       
+        oscillator.connect(gainNode);
+    
+        analyser = context.createAnalyser();
+        
+        gainNode.connect(analyser)
+        analyser.connect(context.destination);
+
+        oscillator.start();
+        draw();
+    }
+
+    function draw() {
+        analyser.fftSize = 2048;
+        var bufferLength = analyser.frequencyBinCount;
+        var dataArray = new Uint8Array(bufferLength);
+        analyser.getByteTimeDomainData(dataArray);
+    
+        var canvas = document.getElementById("oscilloscope");
+        canvas.width = document.body.clientWidth;
+        var canvasCtx = canvas.getContext("2d");
+
+        requestAnimationFrame(draw);
+
+        canvasCtx.fillStyle = "#fff8dc";
+        canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+        canvasCtx.lineWidth = 2;
+        canvasCtx.strokeStyle = "#ff6969";
+        canvasCtx.beginPath();
+
+        var sliceWidth = canvas.width / bufferLength;
+        var x = 0;
+
+        for (var i = 0; i < bufferLength; i++) {
+            var v = dataArray[i] / 128.0;
+            var y = v * canvas.height / 2;
+            if (i === 0) {
+            canvasCtx.moveTo(x, y);
+            } else {
+            canvasCtx.lineTo(x, y);
+            }
+            x += sliceWidth;
+        }
+        canvasCtx.lineTo(canvas.width, canvas.height / 2);
+        canvasCtx.stroke();
+    }
 
     document.querySelector('.volume').addEventListener('input', function() {
-        this.classList.add('active')
-        gainNode.gain.value = this.value
+        if (gainNode !== undefined && document.querySelector('.start').classList.contains('active')) {
+            const value = Number(this.value);
+            gainNode.gain.setValueAtTime(value, context.currentTime);
+        }
     });
 
+
     document.querySelector('.change-waveform').addEventListener('click', function() {
-        let display = document.querySelector('.display');
+        const display = document.querySelector('.display');
         const types = ['sine', 'triangle', 'sawtooth', 'square'];
-        
+
         if (types.indexOf(this.value) + 1 === types.length) {
             this.value = types[0];
             oscillator.type = types[0];
@@ -57,76 +102,49 @@ window.onload = function() {
         }
     });
 
-
-
     var keys = document.querySelectorAll('.key');
-    
+   
     for(var i = 0, max = keys.length; i < max; i++) {
         keys[i].onclick = function() {
-            oscillator.frequency.value = 261.63 * Math.pow(2, this.value/12);
+            if (document.querySelector('.start').classList.contains('active')) {
+                oscillator.frequency.value = octave * Math.pow(2, this.value/12);
+            }
         }
 
         keys[i].onmouseover = function(e) {
-            if (e.buttons === 1){
-                oscillator.frequency.value = 261.63 * Math.pow(2, this.value/12);
+            if (e.buttons === 1 && document.querySelector('.start').classList.contains('active')){
+                oscillator.frequency.value = octave * Math.pow(2, this.value/12);
             }
         }
     }
 
-    // oscilloscope
-    analyser.fftSize = 2048;
+    var octaves = document.querySelectorAll('.octaves button');
 
-    //    analyser.fftSize = Math.pow(2, Math.round(Math.log2(canvas.width)));
-    var bufferLength = analyser.frequencyBinCount;
-    var dataArray = new Uint8Array(bufferLength);
-    analyser.getByteTimeDomainData(dataArray);
-
-    var canvas = document.getElementById("oscilloscope");
-    canvas.width = document.body.clientWidth;
-    var canvasCtx = canvas.getContext("2d");
-
-    // draw an oscilloscope of the current audio source
-
-    function draw() {
-
-    requestAnimationFrame(draw);
-
-    analyser.getByteTimeDomainData(dataArray);
-
-
-    canvasCtx.fillStyle = "#fff8dc";
-
-    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-
-    canvasCtx.lineWidth = 2;
-    canvasCtx.strokeStyle = "#ff6969";
-
-    canvasCtx.beginPath();
-
-    var sliceWidth = canvas.width * 1.0 / bufferLength;
-    var x = 0;
-
-    for (var i = 0; i < bufferLength; i++) {
-
-        var v = dataArray[i] / 128.0;
-        var y = v * canvas.height / 2;
-
-        if (i === 0) {
-        canvasCtx.moveTo(x, y);
-        } else {
-        canvasCtx.lineTo(x, y);
+    for(var i = 0, max = octaves.length; i < max; i++) {
+        octaves[i].onclick = function() {
+            octave = this.value;
+            octaves.forEach(octave => octave.classList.remove('active'));
+            this.classList.add('active');
         }
-
-        x += sliceWidth;
     }
 
-    canvasCtx.lineTo(canvas.width, canvas.height / 2);
-    canvasCtx.stroke();
+    function setupCanvas() {
+        var canvas = document.getElementById("oscilloscope");
+        canvas.width = document.body.clientWidth;
+        var canvasCtx = canvas.getContext("2d");
+    
+        canvasCtx.fillStyle = "#fff8dc";
+        canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+        canvasCtx.lineWidth = 2;
+        canvasCtx.strokeStyle = "#ff6969";
+        canvasCtx.beginPath();
+    
+        const y = canvas.height / 2;
+        canvasCtx.moveTo(0, y);
+        canvasCtx.lineTo(canvas.width, y);
+        canvasCtx.stroke();
     }
-
-draw();
-
-  }
+}
 
 
 
